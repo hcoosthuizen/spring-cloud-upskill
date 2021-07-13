f5af03297cf69b7ed7ebf6356316448408ac922b
Huxley Oosthuizen <huxley.oosthuizen@entelect.co.za>
2021-07-13 09:10:53 +0200

    Initial commit
    - Empty angular app
    - Eureka, config and gateway servers

---
+++ config-server/src/main/java/com/thegroup/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.thegroup.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.support.GitCredentialsProviderFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
