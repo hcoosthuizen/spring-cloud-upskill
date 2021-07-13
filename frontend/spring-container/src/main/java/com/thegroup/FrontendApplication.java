511498a784e6121b788f0e9a3b27160aaca5a678
Huxley Oosthuizen <huxley.oosthuizen@entelect.co.za>
2021-07-13 13:45:59 +0200

    Get Angular app running in Spring container

---
+++ frontend/spring-container/src/main/java/com/thegroup/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.thegroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FrontendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+}
