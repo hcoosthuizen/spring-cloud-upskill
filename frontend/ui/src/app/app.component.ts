ae7d59cfef6abab8188131d2422b0c259393414e
Huxley Oosthuizen <huxley.oosthuizen@entelect.co.za>
2021-07-14 14:42:29 +0200

    Fix publishing and use service on UI

--- frontend/ui/src/app/app.component.ts
+++ frontend/ui/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Pokemon} from './api/models/pokemon';
+import {Observable} from 'rxjs';
+import {PokemonControllerService} from './api/services/pokemon-controller.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngui';
+  pokemon: Observable<Array<Pokemon>>;
+
+  constructor(private pokemonService: PokemonControllerService) {
+    this.pokemon = this.pokemonService.getAllPokemon();
+    this.pokemon.subscribe();
+  }
+
+  ngOnInit(): void {
+
+  }
 }
